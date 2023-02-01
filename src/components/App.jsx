391e0be059089cacaccb0aaa990d3bf2f94cf69f
Anna Cihlová <anna.cihlova@gmail.com>
2023-02-01 16:25:53 -0500

    update

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+function App() {
+  return (<div className="layout-wrapper">
+    <section>
+      <Navbar />
+      <h1>Frontend Developer.</h1>
+    </section>
+    <section>
+    <div>
+        <h2>Hello</h2>
+    </div>
+
+    </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Logo from "./Logo";
+
+function Navbar() {
+  return (
+    <div className="nav">
+    <Logo/>
+
+      <ul className="nav-menu">
+        <li className="nav-item">
+          <a href="/">My journey</a>
+        </li>
+        <li className="nav-item">
+          <a href="/">My work</a>
+        </li>
+        <li className="nav-item">
+          <a href="/" className="btn resume">
+          My resume
+          </a>
+        </li>
+      </ul>
+      <div className="hamburger">
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
